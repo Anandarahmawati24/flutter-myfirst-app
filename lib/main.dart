20d5b754069c7f473f6d776796499d3dc5203819
anandarahmawati24 <rahmawatiananda571@gmail.com>
2025-09-28 20:51:02 +0700

    4 adding button next

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,12 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  // ↓ Add this.
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -38,11 +44,9 @@ class MyHomePage extends StatelessWidget {
         children: [
           Text('A random AWESOME idea:'),
           Text(appState.current.asLowerCase),
-
-          // ↓ Add this.
           ElevatedButton(
             onPressed: () {
-              print('button pressed!');
+              appState.getNext(); // ← This instead of print().
             },
             child: Text('Next'),
           ),
